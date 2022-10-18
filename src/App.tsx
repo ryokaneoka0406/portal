b2e64a601e87507c8ac64c454437766d87cf66d2
ryokaneoka0406 <ryo.kaneoka0406@gmail.com>
2022-10-18 22:43:03 +0900

    Add style to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import Avater from "./components/Avater";
 import Contents from "./components/Contents";
 import Footer from "./components/Footer";
+import styled from "styled-components";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Avater />
-      <Contents />
-      <Footer />
+      <BodyWrapper>
+        <Avater />
+        <Contents />
+        <Footer />
+      </BodyWrapper>
     </div>
   );
 }
 
 export default App;
+
+const BodyWrapper = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 28rem;
+  padding: 2rem;
+  @media (min-width: 640px) {
+    max-width: 42rem;
+    padding: 1rem;
+  }
+`;
