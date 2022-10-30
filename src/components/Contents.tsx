c643d2063f268e6c29e63061c27ca7069ef48ed9
ryokaneoka0406 <ryo.kaneoka0406@gmail.com>
2022-10-30 22:01:05 +0900

    Complete v1

--- src/components/Contents.tsx
+++ src/components/Contents.tsx
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 const Contents = () => {
   return (
     <>
@@ -14,6 +16,31 @@ const Contents = () => {
         <li>データ分析、統計</li>
         <li>プログラミング、個人開発</li>
       </ul>
+      <h2>Links</h2>
+      何らか書いたり発表したりしたものです。
+      <ul>
+        <li>
+          <TextLink href="https://schoo.jp/class/8011" target="_blank">
+            データ活用の基礎をつくるKPI設計
+          </TextLink>
+        </li>
+        <li>
+          <TextLink
+            href="https://tech.smarthr.jp/entry/kpi-and-product-vision"
+            target="_blank"
+          >
+            KPIを追いかけていたらプロダクトビジョンにたどり着いた話
+          </TextLink>
+        </li>
+        <li>
+          <TextLink
+            href="https://speakerdeck.com/ryokaneoka0406/xi-hui-kontentu"
+            target="_blank"
+          >
+            チームのコンテキスト理解を高める 朝会/夕会コンテンツ
+          </TextLink>
+        </li>
+      </ul>
       <h2>Contact</h2>
       何かご相談があればTwitterのDMからどうぞ。
     </>
@@ -21,3 +48,8 @@ const Contents = () => {
 };
 
 export default Contents;
+
+const TextLink = styled.a`
+  text-decoration: underline;
+  color: #3182ce;
+`;
